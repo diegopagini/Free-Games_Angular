e6db0bcc0db99a9475c98ebb26bd3f0d8f780d8e
Diego Hernan Pagini <diego.pagini@neoris.com>
2021-12-13 09:00:59 +0100

    core

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './page/home.component';
 
 @NgModule({
   declarations: [HomeComponent],
--- src/app/modules/not-found/not-found.module.ts
+++ src/app/modules/not-found/not-found.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NotFoundComponent } from './not-found.component';
 import { NotFoundRoutingModule } from './not-found-routing.module';
+import { NotFoundComponent } from './page/not-found.component';
 
 @NgModule({
   declarations: [NotFoundComponent],
